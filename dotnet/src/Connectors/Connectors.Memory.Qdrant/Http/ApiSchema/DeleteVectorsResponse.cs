5478d54d368b782bafe82df1991721940a09aa41
piotrek-appstream <129282647+piotrek-appstream@users.noreply.github.com>
2023-07-14 14:19:02 +0530

    DeleteVectorResponse added so QdrantVectorDbClient can deserialize it

---
+++ dotnet/src/Connectors/Connectors.Memory.Qdrant/Http/ApiSchema/DeleteVectorsResponse.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.SemanticKernel.Connectors.Memory.Qdrant.Http.ApiSchema;
+
+internal sealed class DeleteVectorsResponse : QdrantResponse
+{
+}
